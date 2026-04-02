a5e63a249dc11c2ece1e8d3d998b2e970f15b5be
weber12599 <weber12599@gmail.com>
2026-04-02 19:13:22 +0800

    [Refactor] Remove HomeView component and update router configuration

--- quizio-game/frontend/src/router/index.ts
+++ quizio-game/frontend/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import HostView from '../views/HostView.vue'
 import StudentRoomView from '../views/StudentRoomView.vue'
 import ScreenView from '../views/ScreenView.vue'
@@ -8,7 +7,6 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', name: 'root', redirect: 'join' },
-        { path: '/home', name: 'home', component: HomeView },
         { path: '/host', name: 'host', component: HostView },
         { path: '/screen', name: 'screen', component: ScreenView },
         { path: '/join', name: 'join', component: StudentRoomView }
