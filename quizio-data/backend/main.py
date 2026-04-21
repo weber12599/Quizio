a612619f12fb7eb72cdea3c24bce6ca8e28ce3bd
weber12599 <weber12599@gmail.com>
2026-04-21 23:55:43 +0800

    [Release] Release v0.1.4

--- quizio-data/backend/main.py
+++ quizio-data/backend/main.py
@@ -77,7 +77,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title='Quizio Data API',
     description='API document for Quizio Data',
-    version='v0.1.3',
+    version='0.1.4',
     lifespan=lifespan,
 )
 
