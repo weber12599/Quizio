b24bc86ee57d354caa8ece2195285761e295835c
weber12599 <weber12599@gmail.com>
2026-04-28 20:37:06 +0800

    [Release] Release v0.2.1

--- quizio-game/backend/main.py
+++ quizio-game/backend/main.py
@@ -6,7 +6,7 @@ from sockets import sio
 fastapi_app = FastAPI(
     title='Quizio Game API',
     description='API document for Quizio Game',
-    version='0.2.0',
+    version='0.2.1',
 )
 fastapi_app.include_router(api_router)
 
